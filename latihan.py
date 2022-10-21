c6ea197e939bd426d3b99ea81947988bc99de3db
Metta1218 <mettatriana@student.telkomuniversity.ac.id>
2022-10-21 07:49:34 +0700

    tes

---
+++ latihan.py
@@ -0,0 +1,27 @@
+'''
+import numpy as np
+
+
+
+endorse=[10000,230000,400000,500000,200000]
+min= np.min(endorse)
+max= np.max(endorse)
+print("range honor endorse di sosmed adalah")
+print(np.std(endorse))
+'''
+'''
+import numpy as np
+
+nilai=[100,98,90,80]
+
+print("nilai terkecil", np.min(nilai))
+print("nilai terbesar", np.max(nilai))
+'''
+
+import numpy as np
+
+investor=[100,200,300,400]
+
+print("mean dari jumlah ivestor",np.mean(investor)  )
+print("jumlah investor terbanyak",np.max(investor))
+
