928d3c72dba32f65e7f7017b92ba126cd82dc034
Metta1218 <91717829+Metta1218@users.noreply.github.com>
2022-10-31 08:54:50 +0700

    Add files via upload

---
+++ modul 0/OOP_OLLA_METTA_1202213248_MODUL1/Database.java
@@ -0,0 +1,24 @@
+public class Database {
+
+    System.out.println(x:"Selamat Datang di Restoran EAD");
+    System.out.println(x:"Silakan register terlebih dahulu");
+    System.out.println(x:"===================================");
+public static void main(String[] args) throws Exception {;
+    }    
+
+    String Insertmenu;
+    String Showmenu;
+    String Searchmenu;
+
+public void InsertMenu (String Insertmenu){
+    this.Insertmenu = Insertmenu;
+}
+
+public void ShowMenu (String Showmenu){
+    this.Showmenu = Showmenu;
+}
+public void SearchMenu (String Searchmenu){
+    this.Searchmenu = Searchtmenu;            
+}     
+
+}
---
+++ modul 0/OOP_OLLA_METTA_1202213248_MODUL1/Menu.java
@@ -0,0 +1,16 @@
+public class Menu {
+    
+String Name;
+String Category;
+String Price;
+
+public void Name (String Name){
+    this.Name = Name;
+}
+
+public void Category (String Category){
+    this.Category = Category;
+}
+public void Price (String Price){
+    this.Price = Price;    
+}
